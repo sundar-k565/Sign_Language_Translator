5afa1452cfeb8b566919676089ad9940522905dc
SundarK <88338291+sundar-k565@users.noreply.github.com>
2025-05-13 00:20:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,100 +2,112 @@ import speech_recognition as sr
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
-from easygui import *
+from easygui import buttonbox
 import os
 from PIL import Image, ImageTk
 from itertools import count
 import tkinter as tk
 import string
-#import selecting
-# obtain audio from the microphone
+
+# Characters supported
+alphabet = list(string.ascii_lowercase)
+
+# Define ImageLabel class once
+class ImageLabel(tk.Label):
+    """A label that displays images, and plays them if they are GIFs"""
+    def load(self, im):
+        if isinstance(im, str):
+            im = Image.open(im)
+        self.loc = 0
+        self.frames = []
+
+        try:
+            for i in count(1):
+                self.frames.append(ImageTk.PhotoImage(im.copy()))
+                im.seek(i)
+        except EOFError:
+            pass
+
+        self.delay = im.info.get('duration', 100)
+
+        if len(self.frames) == 1:
+            self.config(image=self.frames[0])
+        else:
+            self.next_frame()
+
+    def unload(self):
+        self.config(image=None)
+        self.frames = None
+
+    def next_frame(self):
+        if self.frames:
+            self.loc = (self.loc + 1) % len(self.frames)
+            self.config(image=self.frames[self.loc])
+            self.after(self.delay, self.next_frame)
+
+# Load ISL GIF dictionary
+isl_gif = {f.replace(".gif", "").lower(): f for f in os.listdir("ISL_Gifs") if f.endswith(".gif")}
+
+# Main speech-to-sign function
 def func():
-        r = sr.Recognizer()
-        arr=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r', 's','t','u','v','w','x','y','z']
-        with sr.Microphone() as source:
-                r.adjust_for_ambient_noise(source)
-                i=0
-                while True:
-                        print("I am Listening")
-                        audio = r.listen(source)
-                        # recognize speech using Sphinx
-                        try:
-                                a=r.recognize_google(audio)
-                                a = a.lower()
-                                print('You Said: ' + a.lower())
-                                
-                                for c in string.punctuation:
-                                    a= a.replace(c,"")
-                                    
-                                if(a.lower()=='goodbye' or a.lower()=='good bye' or a.lower()=='bye'):
-                                        print("oops!Time To say good bye")
-                                        break
-                                
-                                elif(a.lower() in isl_gif):
-                                    
-                                    class ImageLabel(tk.Label):
-                                            """a label that displays images, and plays them if they are gifs"""
-                                            def load(self, im):
-                                                if isinstance(im, str):
-                                                    im = Image.open(im)
-                                                self.loc = 0
-                                                self.frames = []
+    recognizer = sr.Recognizer()
 
-                                                try:
-                                                    for i in count(1):
-                                                        self.frames.append(ImageTk.PhotoImage(im.copy()))
-                                                        im.seek(i)
-                                                except EOFError:
-                                                    pass
+    with sr.Microphone() as source:
+        recognizer.adjust_for_ambient_noise(source)
+        print("Ready to listen...")
 
-                                                try:
-                                                    self.delay = im.info['duration']
-                                                except:
-                                                    self.delay = 100
+        while True:
+            print("Listening...")
+            try:
+                audio = recognizer.listen(source)
+                text = recognizer.recognize_google(audio).lower()
+                print(f"You said: {text}")
 
-                                                if len(self.frames) == 1:
-                                                    self.config(image=self.frames[0])
-                                                else:
-                                                    self.next_frame()
+                # Clean punctuation
+                text = text.translate(str.maketrans('', '', string.punctuation))
 
-                                            def unload(self):
-                                                self.config(image=None)
-                                                self.frames = None
+                if text in ["goodbye", "good bye", "bye"]:
+                    print("👋 Goodbye!")
+                    break
 
-                                            def next_frame(self):
-                                                if self.frames:
-                                                    self.loc += 1
-                                                    self.loc %= len(self.frames)
-                                                    self.config(image=self.frames[self.loc])
-                                                    self.after(self.delay, self.next_frame)
-                                    root = tk.Tk()
-                                    lbl = ImageLabel(root)
-                                    lbl.pack()
-                                    lbl.load(r'ISL_Gifs/{0}.gif'.format(a.lower()))
-                                    root.mainloop()
-                                else:
-                                    for i in range(len(a)):
-                                                    if(a[i] in arr):
-                                            
-                                                            ImageAddress = 'letters/'+a[i]+'.jpg'
-                                                            ImageItself = Image.open(ImageAddress)
-                                                            ImageNumpyFormat = np.asarray(ImageItself)
-                                                            plt.imshow(ImageNumpyFormat)
-                                                            plt.draw()
-                                                            plt.pause(0.8)
-                                                    else:
-                                                            continue
+                elif text in isl_gif:
+                    gif_path = os.path.join("ISL_Gifs", isl_gif[text])
+                    if os.path.exists(gif_path):
+                        root = tk.Tk()
+                        lbl = ImageLabel(root)
+                        lbl.pack()
+                        lbl.load(gif_path)
+                        root.mainloop()
+                    else:
+                        print(f"GIF for '{text}' not found.")
+                else:
+                    for char in text:
+                        if char in alphabet:
+                            img_path = os.path.join("letters", f"{char}.jpg")
+                            if os.path.exists(img_path):
+                                img = Image.open(img_path)
+                                plt.imshow(np.asarray(img))
+                                plt.axis("off")
+                                plt.draw()
+                                plt.pause(0.8)
+                            else:
+                                print(f"Image for '{char}' not found.")
+                    plt.close()
 
-                        except:
-                               print(" ")
-                        plt.close()
-while 1:
-  image   = "signlang.jpg"
-  msg="SIGN LANGUAGE TRANSLATOR"
-  choices = ["START","END"]
-  reply   = buttonbox(msg,image=image,choices=choices)
-  if reply ==choices[0]:
+            except sr.UnknownValueError:
+                print("Could not understand audio.")
+            except sr.RequestError as e:
+                print(f"Error with speech recognition service: {e}")
+            except Exception as e:
+                print(f"Unexpected error: {e}")
+
+# Main Menu
+while True:
+    image = "signlang.jpg"
+    msg = "SIGN LANGUAGE TRANSLATOR"
+    choices = ["START", "END"]
+    reply = buttonbox(msg, image=image, choices=choices)
+    if reply == "START":
         func()
-  if reply == choices[1]:
-        quit()
+    elif reply == "END":
+        break
